7e7bb27a938cde84486dec69a820b021a7a5a004
AayushMohan <aayushmohan1702@gmail.com>
2022-11-15 02:46:47 +0530

    Added: Cards Styling

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Cards = (props: Props) => {
-  return <div className="grid"></div>;
+  return (
+    <div className="grid">
+      <h1 className="text-xl font-semibold">Upcoming Events</h1>
+    </div>
+  );
 };
 
 export default Cards;
